dde8a6b2e7ecf08b3f38b1fcbf4a018001cc8a12
jovinull <felipejovinogamerplay@gmail.com>
2026-01-05 18:50:16 -0300

    feat: http contract

---
+++ contracts/http.ts
@@ -0,0 +1,10 @@
+import type User from '#models/user'
+
+declare module '@adonisjs/core/http' {
+  interface HttpContext {
+    authUser?: User
+    tenant?: {
+      restaurantId: string
+    }
+  }
+}
