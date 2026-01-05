329e33ea8b3ae00caf65d18226e78aa5229cfafd
jovinull <felipejovinogamerplay@gmail.com>
2026-01-05 19:59:23 -0300

    fix: correção na troca de models

--- app/models/user.ts
+++ app/models/user.ts
@@ -1,19 +1,34 @@
 import { DateTime } from 'luxon'
-import { BaseModel, column, hasMany } from '@adonisjs/lucid/orm'
-import type { HasMany } from '@adonisjs/lucid/types/relations'
-import User from '#models/user'
+import { BaseModel, beforeSave, belongsTo, column } from '@adonisjs/lucid/orm'
+import type { BelongsTo } from '@adonisjs/lucid/types/relations'
+import hash from '@adonisjs/core/services/hash'
 
-export default class Restaurant extends BaseModel {
-  static readonly table = 'restaurants'
+import Restaurant from '#models/restaurant'
+import { UserRole } from '#utils/enums'
+
+export default class User extends BaseModel {
+  static readonly table = 'users'
 
   @column({ isPrimary: true })
   declare id: string
 
-  @column()
-  declare name: string
+  @column({ columnName: 'restaurant_id' })
+  declare restaurantId: string
+
+  @belongsTo(() => Restaurant, { foreignKey: 'restaurantId' })
+  declare restaurant: BelongsTo<typeof Restaurant>
 
   @column()
-  declare slug: string
+  declare email: string
+
+  @column()
+  declare role: UserRole
+
+  @column({ serializeAs: null, columnName: 'password_hash' })
+  declare passwordHash: string
+
+  @column({ columnName: 'is_active' })
+  declare isActive: boolean
 
   @column.dateTime({ autoCreate: true })
   declare createdAt: DateTime
@@ -21,6 +36,10 @@ export default class Restaurant extends BaseModel {
   @column.dateTime({ autoCreate: true, autoUpdate: true })
   declare updatedAt: DateTime
 
-  @hasMany(() => User)
-  declare users: HasMany<typeof User>
+  @beforeSave()
+  public static async hashPassword(user: User) {
+    if (user.$dirty.passwordHash) {
+      user.passwordHash = await hash.make(user.passwordHash)
+    }
+  }
 }
